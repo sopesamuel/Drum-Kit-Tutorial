271a8264187db6c6b8fdf2effeb5ba89667bafc6
sopesamuel <104572982+sopesamuel@users.noreply.github.com>
2022-06-06 09:36:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,53 +1,49 @@
-
 var numberOfButtons = document.querySelectorAll("button").length; 
 
 for (var j = 0; j < numberOfButtons; j++) { 
 
-  document.querySelectorAll("button")[j] 
-
-    .addEventListener("click", function() { 
+  document.querySelectorAll("button")[j].addEventListener("click", function() { 
 
       var buttonStyle = this.innerHTML; 
 
       sound(buttonStyle); 
 
-  }
-}
+  });
+} 
 document.addEventListener("keypress", function(event) { 
 
   sound(event.key);
-  }
+  });
   
-
 function sound(key) { 
-  switch (key) { 
+  switch (key) {
     case "S": 
-      var sound1 = new Audio("boom.wav"); 
+      var sound1 = new Audio("kick-bass.mp3"); 
       sound1.play(); 
       break; 
 
     case "A": 
-      var sound2 = new Audio("clap.wav"); 
+      var sound2 = new Audio("sounds_crash.mp3"); 
       sound2.play(); 
       break; 
 
     case "M": 
-      var sound3 = new Audio("hihat.wav"); 
+      var sound3 = new Audio("sounds_snare.mp3"); 
       sound3.play(); 
       break; 
 
     case "U": 
-      var sound4 = new Audio("kick.wav"); 
+      var sound4 = new Audio("sounds_tom-1.mp3"); 
       sound4.play(); 
       break; 
 
     case "E": 
-      var sound5 = new Audio("openhat.wav"); 
+      var sound5 = new Audio("sounds_tom-2.mp3"); 
       sound5.play(); 
       break; 
 
     case "L": 
-      var sound6 = new Audio("ride.wav"); 
+      var sound6 = new Audio("sounds_tom-3.mp3"); 
       sound6.play(); 
       break; 
 
